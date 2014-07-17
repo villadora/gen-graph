18d60474595fc88d159031e0071c7b94575486c1
Wei Gao <jky239@gmail.com>
2014-07-17 15:44:45 +0800

    identify the isophormis nodes

--- index.js
+++ index.js
@@ -1,98 +1,121 @@
 "use strict";
 var Multimap = require('multimap');
 
-var EDGE_KEYS = require('./lib/common').EDGE_KEYS;
 
+var EDGE_KEYS = require('./lib/common').EDGE_KEYS;
+var isIsomorphism = require('./lib/iso');
 
 
 module.exports = function(trees, options) {
+  gid.__id__ = 0;
+
   var map = new Multimap();
+  var rs = {
+    _: {}
+  };
+
+  options = options || {};
+  var edgeKeys = options.edge_keys || EDGE_KEYS;
+
+  if (!Array.isArray(trees))
+    trees = [trees];
+
   trees.forEach(function(root) {
-    travel(root, map);
+    travel(root.name, root);
+    rs._[root.name + '@' + root.version] = gid(root);
   });
-}
 
 
-function travel(root, map, path) {
-  path = path || [];
+  return rs;
 
-  var id = root.name + '@' + root.version;
+  function travel(name, root, path) {
+    path = path || [];
 
-  if (~path.indexOf(id)) {
-    // cycle
-
-  }
-
-}
+    var id = name + '@' + root.version;
 
 
-
-
-function isIsomorphism(a, b, path) {
-  path = path || [];
-  var aid = a.name + '@' + a.version;
-  var bid = b.name + '@' + b.version;
-  if (aid == bid) {
-
-    if (~path.indexOf(aid)) {
-      return true;
+    var cycle = false;
+    for (var i = 0, len = path.length; i < len; ++i) {
+      if (path[i].id == id) {
+        root.__id = path[i].oid;
+        cycle = true;
+        break;
+      }
     }
 
-    path.push(aid);
-    var compare = true;
-    EDGE_KEYS.forEach(function(depName) {
-      var da = a[depName],
-        db = b[depName];
-      if (!compare)
-        return;
+    if (cycle)
+      return;
 
-      if (da && db) {
-        var das = Object.keys(da).sort(),
-          dbs = Object.keys(dbs).sort();
+    var nodes = map.get(id),
+      found = false;
 
-        if (das.length != dbs.length)
-          compare = false;
-        else {
-          for (var i = 0, len = das.length; i < len && compare; ++i) {
-            var aname = das[i],
-              bname = dbs[i];
-            if (aname != bname)
-              compare = false;
-            else {
-              var ma = da[aname],
-                mb = db[bname];
-              if (ma.version != mb.version) {
-                compare = false;
-              } else {
-                compare = isIsomorphism(ma, mb, path);
-              }
-            }
+    (nodes || []).forEach(function(node) {
+        if (!found) {
+          if (isIsomorphism(root, node)) {
+            root.__id = gid(node);
+            found = true;
           }
         }
-      } else {
-        compare = false;
+      });
+
+    if (!found) {
+      map.set(id, root);
+    }
+
+    var oid = gid(root);
+
+    if (rs.hasOwnProperty(oid)) {
+      // no need to handle isomophismes of an graph
+      return;
+    }
+
+
+    path.push({
+      id: id,
+      oid: oid
+    });
+
+    edgeKeys.forEach(function(depName) {
+      var deps = root[depName];
+      if (deps) {
+        Object.keys(deps).forEach(function(name) {
+          travel(name, deps[name], path);
+        });
       }
     });
 
-    path.pop();
 
-    return compare;
+    // post order travel
+    var obj = rs[oid] = [id];
+    var dep;
+
+    edgeKeys.forEach(function(depName) {
+      var deps = root[depName];
+      if (deps) {
+        dep = dep || {};
+        Object.keys(deps).forEach(function(name) {
+          var node = deps[name];
+          dep[node.from] = gid(node);
+        });
+      }
+    });
+
+    if (dep)
+      obj.push(dep);
+
+    path.pop();
   }
 
-  return false;
-}
 
-isIsomorphism.cache = new Multimap();
+  function gid(object) {
+    if (object.hasOwnProperty('__id')) {
+      return object.__id;
+    }
 
+    object.__id = gid.__id__++;
 
-
-function gid(object) {
-  if (object.__id) {
     return object.__id;
   }
 
-  id.__id__++;
-  return object.__id = id.__id__;
-}
 
-id.__id__ = 0;
+};
